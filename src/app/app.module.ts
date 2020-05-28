aab7a4c3e74c74c434e143573029b1996447a594
michał frankiewicz <wasyl144@outlook.com>
2020-05-28 18:03:04 +0200

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,19 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    PageNotFoundComponent,
     
   ],
   imports: [
