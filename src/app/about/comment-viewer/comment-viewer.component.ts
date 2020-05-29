5b7593bea5f13ff20dd5add0db51627a44ed1dda
michał frankiewicz <wasyl144@outlook.com>
2020-05-29 16:29:57 +0200

    About Component works

--- src/app/about/comment-viewer/comment-viewer.component.ts
+++ src/app/about/comment-viewer/comment-viewer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentsService } from '../comments.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Comments } from '../Comments'
 
 @Component({
   selector: 'app-comment-viewer',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentViewerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private commentsService:CommentsService) { }
+
+  dataSource = new MatTableDataSource<Comments>();
+  buffer:Comments[] = [];
 
   ngOnInit(): void {
+    this.commentsService.getData().subscribe(info => {
+      if (info.nickname != '') { //aby nie dodawało do naszej tablicy pustego elementu
+        this.buffer.push(info);
+        this.dataSource.data=this.buffer;// "pushujemy" dane do naszej tablicy
+        console.log(this.dataSource);
+      }
+    })
   }
 
   displayedColumns: string[] = ['Nickname', 'E-Mail', 'Comment']
