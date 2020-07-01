7cb957ee15fa70c3baf2e52d3a069e5a24a05cdf
michał frankiewicz <wasyl144@outlook.com>
2020-07-01 12:50:37 +0200

    Added loader, now errors gone

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,36 +4,36 @@ import { WeatherService } from './weather.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private weatherService: WeatherService) { }
+  constructor(private weatherService: WeatherService) {}
 
   weatherData: any; // zmienna na dane
   errorData: any;
 
   show: boolean = false;
+  showBar: boolean = false;
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   onSubmit(city: string) {
-    this.show=false; // resetuje te zmienne aby uniknąć błędów
+    this.show = false; // resetuje te zmienne aby uniknąć błędów
+    this.showBar = true;
     this.errorData = null;
-    setTimeout(() => [
-      this.weatherService.getWeather(city).subscribe(data => { //dane przypisujemy do naszej zmiennej, nasłuchuje zmianny zmiennej
+    this.weatherService.getWeather(city).subscribe(
+      (data) => {
+        //dane przypisujemy do naszej zmiennej, nasłuchuje zmianny zmiennej
         this.weatherData = data;
+        this.show = true;
+        this.showBar = false;
       },
-        err => { //dane odnośnie errorów przypisuje do zmiennej
-          this.errorData = err;
-        }),
-    ], 500)
-    setTimeout( ()=>[ // Lazy loading
-      this.show = true
-    ],1000
-    )
-    
+      (err) => {
+        //dane odnośnie errorów przypisuje do zmiennej
+        this.errorData = err;
+        this.show = true;
+        this.showBar = false;
+      }
+    );
   }
 }
